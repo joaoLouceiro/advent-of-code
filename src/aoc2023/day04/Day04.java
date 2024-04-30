814f1c5904deb4dbb2f4dd9505b7c73220ff60cb
joaoLouceiro <jrlouceiro@live.com>
2024-04-30 10:38:02 +0100

    Fix pt1
    - Changed Regex for pt2, didn't adapt pt1
    - Static file declaration and initialization

--- src/aoc2023/day04/Day04.java
+++ src/aoc2023/day04/Day04.java
@@ -14,6 +14,8 @@ import aoc2023.SuperDay;
 
 public class Day04 extends SuperDay {
 
+    
+    private static File file = new File("./src/aoc2023/day04/files/Input.txt");
     private static Pattern p = Pattern.compile("(?:Game \\d+:)|(\\d+)");
     private static List<String> scratchList = new ArrayList<>();
     private static int p2Total = 0;
@@ -25,21 +27,18 @@ public class Day04 extends SuperDay {
     }
 
     private void part1() throws FileNotFoundException {
-        File file = new File("./src/aoc2023/day04/files/Input.txt");
         Scanner sc = new Scanner(file);
-        // Pattern p = Pattern.compile("(\\d+)");
         int total = 0;
         while (sc.hasNextLine()) {
             String s = sc.nextLine();
             Matcher m = p.matcher(s);
             Map<String, Boolean> map = new HashMap<String, Boolean>();
             int gameTotal = 0;
+            // Get "Game : x:" match out of the way
+            m.find();
             for (int i = 0; m.find(); i++) {
                 String gString = m.group(0);
-                if (i == 0) {
-                    continue;
-                }
-                if (gString != null && i <= 10) {
+                if (gString != null && i < 10) {
                     map.put(gString, true);
                 } else {
                     if (map.get(gString) != null)
@@ -53,7 +52,6 @@ public class Day04 extends SuperDay {
     }
 
     private void part2() throws FileNotFoundException {
-        File file = new File("./src/aoc2023/day04/files/Input.txt");
         Scanner sc = new Scanner(file);
         while (sc.hasNextLine()) {
             String s = sc.nextLine();
