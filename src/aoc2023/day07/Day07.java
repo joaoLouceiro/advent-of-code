7d6c9864b548a7342b807eb6b99809c36016c6c0
joaoLouceiro <jrlouceiro@live.com>
2024-05-29 08:31:08 +0100

    Day 07, pt 2 (WIP)

--- src/aoc2023/day07/Day07.java
+++ src/aoc2023/day07/Day07.java
@@ -2,6 +2,7 @@ package aoc2023.day07;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,7 +18,8 @@ public class Day07 extends Calendar {
 
   private String filename = "./src/aoc2023/day07/files/Input.txt";
   private static Map<Character, Integer> cardValueMap = new HashMap<>();
-  static {
+
+  private static void fillCardMapPart1() {
     cardValueMap.put('A', 13);
     cardValueMap.put('K', 12);
     cardValueMap.put('Q', 11);
@@ -33,6 +35,23 @@ public class Day07 extends Calendar {
     cardValueMap.put('2', 1);
   }
 
+  private static void fillCardMapPart2() {
+    cardValueMap.put('A', 13);
+    cardValueMap.put('K', 12);
+    cardValueMap.put('Q', 11);
+    cardValueMap.put('T', 10);
+    cardValueMap.put('9', 9);
+    cardValueMap.put('8', 8);
+    cardValueMap.put('7', 7);
+    cardValueMap.put('6', 6);
+    cardValueMap.put('5', 5);
+    cardValueMap.put('4', 4);
+    cardValueMap.put('3', 3);
+    cardValueMap.put('2', 2);
+    cardValueMap.put('J', 1);
+
+  }
+
   /**
    * Parse the document
    * Build HandBidMap<String, Integer>
@@ -45,6 +64,7 @@ public class Day07 extends Calendar {
   @Override
   public void run() throws FileNotFoundException {
     System.out.println();
+    fillCardMapPart2();
     long startRun = System.currentTimeMillis();
     Scanner sc = super.getScanner(filename);
     Map<Integer, ArrayList<HandBidMap>> rankedHandMap = buildRankedHandMap(sc);
@@ -77,10 +97,11 @@ public class Day07 extends Calendar {
       int start,
       int end,
       int charIndex) {
-    List<HandBidMap> newList = list.subList(start, end);
-    newList.sort((a, b) -> {
-      return cardValueMap.get(a.getKey().charAt(charIndex)) - cardValueMap.get(b.getKey().charAt(charIndex));
-    });
+    // List<HandBidMap> newList = list.subList(start, end);
+    // newList.sort((a, b) -> {
+    // return cardValueMap.get(a.getKey().charAt(charIndex)) -
+    // cardValueMap.get(b.getKey().charAt(charIndex));
+    // });
 
     list.subList(start, end).sort((a, b) -> {
       return cardValueMap.get(a.getKey().charAt(charIndex)) - cardValueMap.get(b.getKey().charAt(charIndex));
@@ -121,12 +142,38 @@ public class Day07 extends Calendar {
     while (sc.hasNext()) {
       String hand = sc.next();
       int bid = sc.nextInt();
-      int handValue = getHandValue(hand);
+      int handValue = getHandValuePart2(hand);
       ArrayList<HandBidMap> handList = rankedHandMap.getOrDefault(handValue, new ArrayList<HandBidMap>());
       handList.add(new HandBidMap(hand, bid));
       rankedHandMap.put(handValue, handList);
     }
     return rankedHandMap;
+  }
+
+  private int getHandValuePart2(String hand) {
+    Map<Character, Integer> mp = new HashMap<Character, Integer>();
+    int jokerCount = 0;
+    for (int i = 0; i < 5; i++) {
+      char c = hand.charAt(i);
+      if (c == 'J') {
+        jokerCount += 1;
+      } else {
+        mp.put(c, mp.getOrDefault(c, 1) * 4);
+      }
+    }
+
+    if (jokerCount == 5) {
+      mp.put('J', (int) Math.pow(4, jokerCount));
+    } else if (jokerCount > 0) {
+      char maxKey = Collections.max(mp.entrySet(), Map.Entry.comparingByValue()).getKey();
+      mp.put(maxKey, mp.get(maxKey) * jokerCount * 4);
+    }
+    int total = 0;
+
+    for (char c : mp.keySet()) {
+      total += mp.get(c);
+    }
+    return total;
   }
 
   private int getHandValue(String hand) {
