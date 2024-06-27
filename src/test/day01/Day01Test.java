81cefa3c7764762096f57a2932461207480dd596
joaoLouceiro <jrlouceiro@live.com>
2024-06-27 21:13:36 +0100

    Add JUnit (WIP)

---
+++ src/test/day01/Day01Test.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Day01Test {
+  @Test
+  public void sumTest() {
+  }
+
+  @Test
+  public void sumTest2() {
+  }
+}
